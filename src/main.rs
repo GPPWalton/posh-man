2f265544ac436e06d92c95f1468da2f235810fcc
Gabriel Walton <gabrielppwalton@gmail.com>
2025-08-26 11:46:23 +0100

    feat: rough skeleton of project object added

---
+++ src/main.rs
@@ -0,0 +1,12 @@
+use std::io;
+use project::project::create_project;
+fn main() {
+    // let mut guess = String::new();
+
+    // println!("Guess: ");
+    // io::stdin()
+    //     .read_line(&mut guess)
+    //     .expect("Failed to read line");
+    // println!("{}",guess)
+    let test_project = create_project();
+}
