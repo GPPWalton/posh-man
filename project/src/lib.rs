4c868cfe6763d31e4bf6b77aaa7d365492cecd64
Gabriel Walton <gabrielppwalton@gmail.com>
2025-09-15 13:56:46 +0100

    feat: renamed project::create_project to create_from_existing

--- project/src/lib.rs
+++ project/src/lib.rs
@@ -22,18 +22,21 @@ pub mod project{
         status: bool,
 
     }
-    pub fn create_project() -> Project{
+    pub fn create_from_existing(project_name: &str, size: u8,
+        cost: Cost, whole_army: bool, needs_assembly: bool,
+        kitibash_rating: u8, paint_level: PaintLevel, priority: f64,
+        status: bool) -> Project{
         let test = Project{
-        project_name: "Dark Angels Centurion".to_string(),
-        size: 1,
-        cost: Cost::None,
-        whole_army: false,
-        needs_assembly: false,
-        kitbash_rating: 1,
-        paint_level: PaintLevel::Character,
-        priority: 1.0,
-        status: true,
-    };
+            project_name: project_name.to_string(),
+            size: size,
+            cost: cost,
+            whole_army: whole_army,
+            needs_assembly: needs_assembly,
+            kitbash_rating: kitibash_rating,
+            paint_level: paint_level,
+            priority: priority,
+            status: status,
+        };
     return  test;
     }
 }
