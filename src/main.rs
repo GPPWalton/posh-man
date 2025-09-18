0ba8f4cc0ed06bcf47489bbea448239bf55744e0
Gabriel Walton <gabrielppwalton@gmail.com>
2025-09-18 14:32:41 +0100

    feat: missing assignment shorthand added to lib.rs

--- src/main.rs
+++ src/main.rs
@@ -16,9 +16,13 @@ fn new_file(file_path: Option<&str>)-> Result<File, Box<dyn Error>>{
     let file = File::create(file_path.unwrap())?;
    let mut wtr = csv::Writer::from_path(file_path.unwrap())?;
     // Add header to file
-    wtr.write_record(&["Project","Size","Cost","Whole Army/Warband",
-    "Assembly Required","Kitbash rating","Painting level","Complexity rating",
-    "Preference modifier","Priority","Status","Is Owned"])?;
+    // old header-> keep for later 'prettying-up'
+    // wtr.write_record(&["Project","Size","Cost","Whole Army/Warband",
+    // "Assembly Required","Kitbash rating","Painting level","Complexity rating",
+    // "Preference modifier","Priority","Status","Is Owned"])?;
+    wtr.write_record(&["project_name", "size","cost", "whole_army",
+                        "needs_assembly", "kitbash_rating", "paint_level",
+                        "complexity_rating","preference_modifier","priority", "status",])?;
 
     wtr.flush()?;
     Ok(file)
@@ -38,9 +42,16 @@ fn read_file()-> Result<(), Box<dyn Error>>{
     let mut rdr = csv::Reader::from_reader(file);
 
     // Loop over each record.
-    for result in rdr.records() {
-        // An error may occur, so abort the program in an unfriendly way.
-        let record =result?;
+    // for result in rdr.records() {
+    //     // An error may occur, so abort the program in an unfriendly way.
+    //     let record =result?;
+    //     // Print a debug version of the record.
+    //     println!("{:?}", record);
+    // }
+
+    //TODO:this should work to parse the records, but you'll need to rename/change headers or struct fields!
+    for result in rdr.deserialize(){
+        let record: Project = result?;
         // Print a debug version of the record.
         println!("{:?}", record);
     }
@@ -72,7 +83,6 @@ fn get_first_arg() -> Result<(), Box<dyn Error>>  {
 }
 
 fn main() {
-    //TODO:Parse CSV lines into Struct format
     // let test_project: Project = create_from_existing();
       if let Err(err) = get_first_arg() {
         println!("{}", err);
