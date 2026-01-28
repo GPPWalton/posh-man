6d99e27c56a00f18f0a14d1802425bbd61a32a6f
Gabriel Walton <gabrielppwalton@gmail.com>
2026-01-28 18:06:13 +0000

    chore: tidied up extraneous functions from pre-tabled implementation, moved headers array directly to new_file function

--- src/main.rs
+++ src/main.rs
@@ -9,7 +9,7 @@ use project::project::Project;
 use csv;
 use::tabled::{Table};
 
-fn new_file(file_path: Option<&str>, headers: &[&str])-> Result<File, Box<dyn Error>>{
+fn new_file(file_path: Option<&str>, headers: [&str; 12])-> Result<File, Box<dyn Error>>{
     //create a File using file_path
     let file = File::create(file_path.unwrap())?;
    let mut wtr = csv::Writer::from_path(file_path.unwrap())?;
@@ -20,8 +20,13 @@ fn new_file(file_path: Option<&str>, headers: &[&str])-> Result<File, Box<dyn Er
     Ok(file)
 }
 
-fn read_file(headers: &[&str])-> Result<(Vec<Project>), Box<dyn Error>>{
+fn read_file()-> Result<(Vec<Project>), Box<dyn Error>>{
     let filepath= OsString::from("project_priorities.csv");
+
+    let headers = ["Project","Size","Cost","Whole Army/Warband",
+    "Assembly Required","Kitbash Rating","Painting Level","Complexity Rating",
+    "Preference Modifier","Priority","Status","Is Owned"];
+
     //if file does not exist, make a new one!
     let file = File::open(&filepath).unwrap_or_else(|error| {
         if error.kind() == ErrorKind::NotFound {
@@ -49,20 +54,7 @@ fn add_project() -> Result<(), Box<dyn Error>> { //IMPLEMENT: add record to proj
     Ok(())
 }
 
-fn flatten_headers (headers: &[&str])-> String{
-    //flatten headers into a single string with tabs to space out each element.
-    let flat_headers: String = headers.iter()
-                            .map(|s| format!("{} \t", s))
-                            .collect();
-    println!("\u{001b}[37;1m {} \x1B[22m", flat_headers);
-    flat_headers
-}
-
-
-fn render_table(data: Vec<Project>, headers: &[&str]) {
-    //flatten headers
-    let flat_headers =flatten_headers(headers);
-
+fn render_table(data: Vec<Project>) {
     //iterate through each element in data, adding escape characters
     let table = Table::new(data);
 
@@ -72,14 +64,14 @@ fn render_table(data: Vec<Project>, headers: &[&str]) {
     //then add a function to display basic metrics.
 }
 
-fn get_first_arg(headers: &[&str]) -> Result<(), Box<dyn Error>>  {
+fn get_first_arg() -> Result<(), Box<dyn Error>>  {
     //maybe change to accept different arguments, display to show project priorities table,
     match env::args_os().nth(1) {
         None => Err(From::from("expected 1 argument, but got none")),
         Some(argument )=> {if argument == OsString::from("display") {
             //run function for displaying csv
-            let data = read_file(headers)?;
-            render_table(data, headers);
+            let data = read_file()?;
+            render_table(data);
             Ok(())
         }
         //UNIMPLEMENTED
@@ -93,11 +85,8 @@ fn get_first_arg(headers: &[&str]) -> Result<(), Box<dyn Error>>  {
 }
 
 fn main() {
-    let headers = ["Project","Size","Cost","Whole Army/Warband",
-    "Assembly Required","Kitbash Rating","Painting Level","Complexity Rating",
-    "Preference Modifier","Priority","Status","Is Owned"];
     // let test_project: Project = create_from_existing();
-      if let Err(err) = get_first_arg(&headers) {
+      if let Err(err) = get_first_arg() {
         println!("{}", err);
         process::exit(1);
     }
