509b5ea821d3ac529bb9f65430d7f422e6d08873
Gabriel Walton <gabrielppwalton@gmail.com>
2026-02-10 15:19:49 +0000

    feat(display): ratatui stateful table  implemented, functionality WIP

---
+++ src/app.rs
@@ -0,0 +1,348 @@
+
+use std::io;
+
+use project::project::Project;
+use crossterm::event::{self, Event as crossEvent, KeyCode, KeyEvent, KeyEventKind};
+use ratatui::{
+    layout::Constraint,
+    buffer::Buffer,
+    layout::{Layout, Rect,Margin},
+    style::{Style, Stylize, Modifier,palette::tailwind, Color},
+    symbols::border,
+    text::{Line, Text},
+    widgets::{Block, Paragraph,BorderType, Table,
+        TableState, Row, Cell,
+        ScrollbarState, HighlightSpacing,
+        Scrollbar,ScrollbarOrientation,},
+    DefaultTerminal, Frame,
+};
+
+use unicode_width::UnicodeWidthStr;
+
+const PALETTES: [tailwind::Palette; 4] = [
+    tailwind::BLUE,
+    tailwind::EMERALD,
+    tailwind::INDIGO,
+    tailwind::RED,
+];
+#[derive(Debug, Default)]
+struct TableColors {
+    buffer_bg: Color,
+    header_bg: Color,
+    header_fg: Color,
+    row_fg: Color,
+    selected_row_style_fg: Color,
+    selected_column_style_fg: Color,
+    selected_cell_style_fg: Color,
+    normal_row_color: Color,
+    alt_row_color: Color,
+    footer_border_color: Color,
+}
+
+impl TableColors {
+    const fn new(color: &tailwind::Palette) -> Self {
+        Self {
+            buffer_bg: tailwind::SLATE.c950,
+            header_bg: color.c900,
+            header_fg: tailwind::SLATE.c200,
+            row_fg: tailwind::SLATE.c200,
+            selected_row_style_fg: color.c400,
+            selected_column_style_fg: color.c400,
+            selected_cell_style_fg: color.c600,
+            normal_row_color: tailwind::SLATE.c950,
+            alt_row_color: tailwind::SLATE.c900,
+            footer_border_color: color.c400,
+        }
+    }
+}
+const ITEM_HEIGHT: usize = 4;
+#[derive(Debug, Default)]
+pub struct App {
+    table_state: TableState,
+    row_pos: usize,
+    data: Vec<Project>,
+    exit: bool,
+    longest_item_lens: (u16, u16, u16,u16, u16, u16,u16, u16, u16),
+    scroll_state: ScrollbarState,
+    colors: TableColors,
+    color_index: usize,
+}
+
+const INFO_TEXT: [&str; 2] = [
+    "<Q> quit | <↑> move up | <↓> move down",
+    "<Enter> select",
+];
+pub enum CurrentScreen {
+    Main,       //main table
+    Editing,    //when editing entry
+    Exiting,    //confirmation of edit
+}
+impl App {
+
+    //constructor
+    pub fn new(data: Vec<Project>) -> App {
+        App {
+            table_state: TableState::default(),
+            row_pos: 0,
+            longest_item_lens: Self::constraint_len_calculator(&data),
+            data: data,
+            exit: false,
+            scroll_state: ScrollbarState::default(),
+            colors: TableColors::new(&PALETTES[0]),
+            color_index: 0 }
+    }
+
+    /// runs the application's main loop until the user quits
+    pub fn run(&mut self, terminal: &mut DefaultTerminal) -> io::Result<()> {
+        while !self.exit {
+            //main loop goes here
+            terminal.draw(|frame| self.draw(frame))?;
+            self.handle_events()?;
+        }
+        Ok(())
+    }
+
+    fn draw(&mut self, frame: &mut Frame) {
+        let vertical = &Layout::vertical([Constraint::Min(5), Constraint::Length(4)]);
+        let rects = vertical.split(frame.area());
+
+        self.set_colors();
+
+        self.render_table(frame, rects[0]);
+        self.render_scrollbar(frame, rects[0]);
+        self.render_footer(frame, rects[1]);
+    }
+
+    fn handle_events(&mut self) -> io::Result<()> {
+        match event::read()? {
+            // it's important to check that the event is a key press event as
+            // crossterm also emits key release and repeat events on Windows.
+            crossEvent::Key(key_event) if key_event.kind == KeyEventKind::Press => {
+                self.handle_key_event(key_event)
+            }
+            _ => {}
+        };
+        Ok(())
+    }
+    fn handle_key_event(&mut self, key_event: KeyEvent) {
+        match key_event.code {
+            KeyCode::Char('q') => self.exit(),
+            KeyCode::Up => self.move_up(),
+            KeyCode::Down => self.move_down(),
+            KeyCode::Enter => self.select_entry(),
+            _ => {}
+        }
+    }
+
+    fn render_table(&mut self, frame: &mut Frame, area: Rect) {
+        let header_style = Style::default()
+            .fg(self.colors.header_fg)
+            .bg(self.colors.header_bg);
+        let selected_row_style = Style::default()
+            .add_modifier(Modifier::REVERSED)
+            .fg(self.colors.selected_row_style_fg);
+        let selected_col_style = Style::default().fg(self.colors.selected_column_style_fg);
+        let selected_cell_style = Style::default()
+            .add_modifier(Modifier::REVERSED)
+            .fg(self.colors.selected_cell_style_fg);
+
+        let header = ["Project","Size","Cost","Whole Army/Warband",
+    "Assembly Required","Kitbash Rating","Painting Level","Complexity Rating",
+    "Preference Modifier","Priority","Status","Is Owned"]
+            .into_iter()
+            .map(Cell::from)
+            .collect::<Row>()
+            .style(header_style)
+            .height(1);
+        let rows = self.data.iter().enumerate().map(|(i, data)| {
+            let color = match i % 2 {
+                0 => self.colors.normal_row_color,
+                _ => self.colors.alt_row_color,
+            };
+            let item = data.as_str_array();
+            item.into_iter()
+                .map(|content| Cell::from(Text::from(format!("\n{content}\n"))))
+                .collect::<Row>()
+                .style(Style::new().fg(self.colors.row_fg).bg(color))
+                .height(4)
+        });
+        let bar = " █ ";
+        let t = Table::new(
+            rows,
+            [
+                // + 1 is for padding.
+                Constraint::Length(self.longest_item_lens.0 + 1),
+                Constraint::Min(self.longest_item_lens.1 + 1),
+                Constraint::Min(self.longest_item_lens.2),
+                Constraint::Min(self.longest_item_lens.3),
+                Constraint::Min(self.longest_item_lens.4),
+                Constraint::Min(self.longest_item_lens.5),
+                Constraint::Min(self.longest_item_lens.6),
+                Constraint::Min(self.longest_item_lens.7),
+                Constraint::Min(self.longest_item_lens.8),
+            ],
+        )
+        .header(header)
+        .row_highlight_style(selected_row_style)
+        .column_highlight_style(selected_col_style)
+        .cell_highlight_style(selected_cell_style)
+        .highlight_symbol(Text::from(vec![
+            "".into(),
+            bar.into(),
+            bar.into(),
+            "".into(),
+        ]))
+        .bg(self.colors.buffer_bg)
+        .highlight_spacing(HighlightSpacing::Always);
+        frame.render_stateful_widget(t, area, &mut self.table_state);
+    }
+
+    fn render_scrollbar(&mut self, frame: &mut Frame, area: Rect) {
+        frame.render_stateful_widget(
+            Scrollbar::default()
+                .orientation(ScrollbarOrientation::VerticalRight)
+                .begin_symbol(None)
+                .end_symbol(None),
+            area.inner(Margin {
+                vertical: 1,
+                horizontal: 1,
+            }),
+            &mut self.scroll_state,
+        );
+    }
+
+    fn render_footer(&self, frame: &mut Frame, area: Rect) {       
+        let info_footer = Paragraph::new(Text::from_iter(INFO_TEXT))
+            .style(
+                Style::new()
+                    .fg(self.colors.row_fg)
+                    .bg(self.colors.buffer_bg),
+            )
+            .centered()
+            .block(
+                Block::bordered()
+                    .border_type(BorderType::Double)
+                    .border_style(Style::new().fg(self.colors.footer_border_color)),
+            );
+        frame.render_widget(info_footer, area);
+    }
+
+    fn exit(&mut self) {
+        self.exit = true;
+    }
+
+    fn move_up(&mut self){
+        //wrap around to bottom
+        //max based on data length, ignoring header and factoring in index of 0
+        let i = match self.table_state.selected() {
+            Some(i) =>{
+                if self.row_pos ==  0 {
+                    self.data.len() -1
+                }
+                else{
+                    i - 1
+                }
+            }
+            None => 0,
+        };
+            self.table_state.select(Some(i));
+            self.scroll_state = self.scroll_state.position(i * ITEM_HEIGHT);
+    }
+
+    fn move_down(&mut self){
+        //wrap around to top  
+        let i = match self.table_state.selected() {
+            Some(i) =>{
+                if self.row_pos ==  self.data.len() -1 {
+                    0
+                }
+                else{
+                    i + 1
+                }}
+            None => 0,
+        };
+            self.table_state.select(Some(i));
+            self.scroll_state = self.scroll_state.position(i * ITEM_HEIGHT);
+        }
+
+    fn select_entry(&mut self){
+        self.color_index = (self.color_index + 1) % PALETTES.len();
+    }
+
+    pub fn next_color(&mut self) {
+        self.color_index = (self.color_index + 1) % PALETTES.len();
+    }
+
+    pub fn previous_color(&mut self) {
+        let count = PALETTES.len();
+        self.color_index = (self.color_index + count - 1) % count;
+    }
+
+    pub fn set_colors(&mut self) {
+        self.colors = TableColors::new(&PALETTES[self.color_index]);
+    }
+
+    fn constraint_len_calculator(items: &Vec<Project>) -> (u16, u16, u16,u16, u16, u16,u16, u16, u16) {
+    //TODO: simplify this as each one is the same before pushing
+    let project_name_len = items
+        .iter()
+        .map(|x| x.project_name())
+        .map(|x| UnicodeWidthStr::width(x))
+        .max()
+        .unwrap_or(0);
+    let size_len = items
+        .iter()
+        .map(|x| x.size().to_string())
+        .map(|x: String| UnicodeWidthStr::width(x.as_str()))
+        .max()
+        .unwrap_or(0);
+    let cost_len = items
+        .iter()
+        .map(|x| x.cost().to_string())
+        .map(|x: String| UnicodeWidthStr::width(x.as_str()))
+        .max()
+        .unwrap_or(0);
+    let whole_army_len = items
+        .iter()
+        .map(|x| x.whole_army().to_string())
+        .map(|x: String| UnicodeWidthStr::width(x.as_str()))
+        .max()
+        .unwrap_or(0);
+    let needs_assembly_len = items
+        .iter()
+        .map(|x| x.needs_assembly().to_string())
+        .map(|x: String| UnicodeWidthStr::width(x.as_str()))
+        .max()
+        .unwrap_or(0);
+    let kitbash_rating_len = items
+        .iter()
+        .map(|x| x.kitbash_rating().to_string())
+        .map(|x: String| UnicodeWidthStr::width(x.as_str()))
+        .max()
+        .unwrap_or(0);
+    let paint_level_len = items
+        .iter()
+        .map(|x| x.paint_level().to_string())
+        .map(|x: String| UnicodeWidthStr::width(x.as_str()))
+        .max()
+        .unwrap_or(0);
+    let priority_len= items
+        .iter()
+        .map(|x| x.priority().to_string())
+        .map(|x: String| UnicodeWidthStr::width(x.as_str()))
+        .max()
+        .unwrap_or(0);
+    let status_len = items
+        .iter()
+        .map(|x| x.status().to_string())
+        .map(|x: String| UnicodeWidthStr::width(x.as_str()))
+        .max()
+        .unwrap_or(0);
+
+    // #[allow(clippy::cast_possible_truncation)]
+    (project_name_len as u16, size_len as u16, cost_len as u16,
+    whole_army_len as u16, needs_assembly_len as u16, kitbash_rating_len as u16,
+    paint_level_len as u16, priority_len as u16, status_len as u16)
+    // (name_len as u16, address_len as u16, email_len as u16)
+}
+}
--- src/main.rs
+++ src/main.rs
@@ -7,14 +7,8 @@ use std::process;
 use std::vec::Vec;
 use project::project::Project;
 use csv;
-use tabled::{Table};
-use tabled::settings::{Style, Modify, object::Rows,
-    Format, object::Columns,Width, Alignment,};
 mod app;
-use crate::{
-    app::{App}
-};
-
+use crate::app::App;
 fn new_file(file_path: Option<&str>, headers: [&str; 12])-> Result<File, Box<dyn Error>>{
     //create a File using file_path
     let file = File::create(file_path.unwrap())?;
@@ -56,33 +50,6 @@ fn add_project() -> Result<(), Box<dyn Error>> { //IMPLEMENT: add record to proj
     Ok(())
 }
 
-fn generate_table (data: Vec<Project>) -> Table {
-    let styling = Style::modern();
-    let mut table = Table::new(data);
-    table.with(styling)
-            .with(Modify::new(Rows::first())
-            //Bold the header row.
-                .with(Format::content(
-                    |s| format!("\u{001b}[37;1m {} \x1B[22m", s))))
-            //wrap project name column if greater than 13 chars long
-            .with(Modify::new(Columns::first()
-                ).with(Width::wrap(13)))
-            //center column contents
-            .with(Modify::new(Columns::new(0..)).with(Alignment::center()));
-    table
-}
-
-fn render_table(data: Vec<Project>) {
-    //iterate through each element in data, adding escape characters
-    let table = generate_table(data);
-
-    println!("{}", table.to_string());
-
-    //once that is done, change certain row backgrounds based on their status
-    //then add afunction to display basic metrics.
-    //create add_entry button / ctrl+A type commands?
-} 
-
 fn get_first_arg() -> Result<(), Box<dyn Error>> {
     //maybe change to accept different arguments, display to show project priorities table,
     match env::args_os().nth(1) {
@@ -92,7 +59,6 @@ fn get_first_arg() -> Result<(), Box<dyn Error>> {
             let data = read_file()?;
 
             Ok(ratatui::run(|terminal| App::new(data).run(terminal))?)
-
         }
         //UNIMPLEMENTED
         else if argument == OsString::from("add"){
