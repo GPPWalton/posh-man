7e68da4dd36d311479b2af4cb00d9ce14d51f152
Gabriel Walton <gabrielppwalton@gmail.com>
2026-02-18 16:26:01 +0000

    feat(add): main screen rendering moved into ui module, getters and setters added to App struct
    feat(add): ui.rs broken down into seperate files within a folder

--- src/app.rs
+++ src/app.rs
@@ -1,56 +1,25 @@
 
-use std::{io, vec};
+use std::{io};
 use project::project::{Project};
 use crossterm::event::{self, Event as crossEvent, KeyCode, KeyEvent, KeyEventKind};
 use ratatui::{
-    DefaultTerminal, Frame, layout::{Constraint, Layout, Margin, Rect},
-    style::{Color, Modifier, Style, Stylize, palette::tailwind},
-     text::{Line, Text}, widgets::{Block, BorderType, Cell, HighlightSpacing,
-        Paragraph, Row, Scrollbar, ScrollbarOrientation, ScrollbarState, Table, TableState}
+    DefaultTerminal, Frame, layout::{Constraint, Layout}
+    ,widgets::{ScrollbarState, TableState}
 };
 use unicode_width::UnicodeWidthStr;
+use crate::ui::{TableColors,PALETTES,main_ui::render_main_ui};
 
-const PALETTES: [tailwind::Palette; 2] = [
-    tailwind::BLUE,
-    tailwind::EMERALD,
-];
-#[derive(Debug, Default)]
-struct TableColors {
-    buffer_bg: Color,
-    header_bg: Color,
-    header_fg: Color,
-    row_fg: Color,
-    selected_row_style_fg: Color,
-    selected_column_style_fg: Color,
-    selected_cell_style_fg: Color,
-    normal_row_color: Color,
-    alt_row_color: Color,
-    footer_border_color: Color,
-}
-
-impl TableColors {
-    const fn new(color: &tailwind::Palette) -> Self {
-        Self {
-            buffer_bg: tailwind::SLATE.c950,
-            header_bg: color.c900,
-            header_fg: tailwind::SLATE.c200,
-            row_fg: tailwind::SLATE.c200,
-            selected_row_style_fg: color.c400,
-            selected_column_style_fg: color.c400,
-            selected_cell_style_fg: color.c600,
-            normal_row_color: tailwind::SLATE.c950,
-            alt_row_color: tailwind::SLATE.c900,
-            footer_border_color: color.c400,
-        }
-    }
-}
 const ITEM_HEIGHT: usize = 4;
 
-//TODO: consider replacing this with a dynamic value calculated in the process_cell_content function
-const CELL_WRAP_LIMIT: u16 = 16;
-const CELL_PADDING: u16 = 1;
-const HEADER_WRAP_LIMIT: u16 = 10;
-
+fn max_width<F, T>(items: &[Project], field_fn: F) -> u16 where F: Fn(&Project) -> T, T: ToString,{
+    items
+        .iter()
+        .map(|x| field_fn(x).to_string())
+        .map(|x| UnicodeWidthStr::width(x.as_str()))
+        .max()
+        //TODO: Eliminate this unwrap_or?
+        .unwrap_or(0) as u16
+}
 pub struct App <'a>{
     table_state: TableState,
     data: Vec<Project>,
@@ -64,10 +33,7 @@ pub struct App <'a>{
     input_array: [&'a str;11],
 }
 
-const INFO_TEXT: [&str; 2] = [
-    "<Q> quit | <↑> move up | <↓> move down",
-    "<Enter> select",
-];
+
 
 pub enum CurrentScreen {
     Main,       //main table
@@ -88,49 +54,8 @@ pub enum CurrentlyEditing {
     ,Status,
     IsOwned
 }
-fn max_width<F, T>(items: &[Project], field_fn: F) -> u16 where F: Fn(&Project) -> T, T: ToString,{
-    items
-        .iter()
-        .map(|x| field_fn(x).to_string())
-        .map(|x| UnicodeWidthStr::width(x.as_str()))
-        .max()
-        //TODO: Eliminate this unwrap_or?
-        .unwrap_or(0) as u16
-}
-fn process_cell_content<'a> (content: String, limit: u16) -> Text<'a>{
-    //find how many lines
-    let isolated_words: Vec<&str> = content.split_whitespace().collect();
 
-    let mut lines: Vec<Line> = Vec::new();
-    let mut current_line = String::new();
-
-    for word in isolated_words {
-        // Check if adding this word would exceed the limit
-        let potential_line = if current_line.is_empty() {
-            word.to_string()
-        } else {
-            format!("{} {}", current_line, word)
-        };
-
-        if potential_line.chars().count() > limit.into() {
-            // Current line is full, push it and start a new line
-            if !current_line.is_empty() {
-                lines.push(Line::from(current_line));
-            }
-            current_line = word.to_string();
-        } else {
-            // Add word to current line
-            current_line = potential_line;
-        }
-    }
-
-    if !current_line.is_empty() {
-        lines.push(Line::from(current_line));
-    }
-    Text::from(lines)
-}
 impl<'a> App<'a> {
-
     //constructor
     pub fn new(data: Vec<Project>) -> App<'a > {
         App {
@@ -151,21 +76,10 @@ impl<'a> App<'a> {
     pub fn run(&mut self, terminal: &mut DefaultTerminal, headers: [&str;11]) -> io::Result<()> {
         while !self.exit {
             //main loop goes here
-            terminal.draw(|frame| self.draw(frame,headers))?;
+            terminal.draw(|frame| render_main_ui(frame, self,headers))?;
             self.handle_events()?;
         }
         Ok(())
-    }
-
-    fn draw(&mut self, frame: &mut Frame, headers: [&str;11]) {
-        let vertical = &Layout::vertical([Constraint::Min(5), Constraint::Length(4)]);
-        let rects = vertical.split(frame.area());
-
-        self.set_colors();
-
-        self.render_table(frame, rects[0],headers);
-        self.render_scrollbar(frame, rects[0]);
-        self.render_footer(frame, rects[1]);
     }
 
     fn handle_events(&mut self) -> io::Result<()> {
@@ -185,101 +99,6 @@ impl<'a> App<'a> {
             KeyCode::Enter => self.select_entry(),
             _ => {}
         }
-    }
-
-    fn render_table(&mut self, frame: &mut Frame, area: Rect,headers: [&str;11]) {
-        let header_style = Style::default()
-            .fg(self.colors.header_fg)
-            .bg(self.colors.header_bg);
-        let selected_row_style = Style::default()
-            .add_modifier(Modifier::REVERSED)
-            .fg(self.colors.selected_row_style_fg);
-        let selected_col_style = Style::default().fg(self.colors.selected_column_style_fg);
-        let selected_cell_style = Style::default()
-            .add_modifier(Modifier::REVERSED)
-            .fg(self.colors.selected_cell_style_fg);
-
-        let header = headers
-            .into_iter()
-            .map(|content| Cell::from(Text::from(
-                process_cell_content(String::from(content),HEADER_WRAP_LIMIT))))
-            .collect::<Row>()
-            .style(header_style)
-            .height(3);
-        let rows = self.data.iter().enumerate().map(|(i, data)| {
-            let color = match i % 2 {
-                0 => self.colors.normal_row_color,
-                _ => self.colors.alt_row_color,
-            };
-            let item = data.as_str_array();
-            //TODO: consider centering each column beyond the first, use slice?
-            item.into_iter()
-                .map(|content| Cell::from(Text::from(process_cell_content(content, CELL_WRAP_LIMIT))))
-                .collect::<Row>()
-                .style(Style::new().fg(self.colors.row_fg).bg(color))
-                .height(4)
-        });
-        let bar = " █ ";
-        let t = Table::new(
-            rows,
-            [
-                Constraint::Min(CELL_WRAP_LIMIT+CELL_PADDING),
-                Constraint::Min(self.longest_item_lens[1].max(HEADER_WRAP_LIMIT)+ CELL_PADDING),
-                Constraint::Min(self.longest_item_lens[2].max(HEADER_WRAP_LIMIT)),
-                Constraint::Min(self.longest_item_lens[3].max(HEADER_WRAP_LIMIT)),
-                Constraint::Min(self.longest_item_lens[4].max(HEADER_WRAP_LIMIT)),
-                Constraint::Min(self.longest_item_lens[5].max(HEADER_WRAP_LIMIT)),
-                Constraint::Min(self.longest_item_lens[6].max(HEADER_WRAP_LIMIT)),
-                Constraint::Min(self.longest_item_lens[7].max(HEADER_WRAP_LIMIT)),
-                Constraint::Min(self.longest_item_lens[8].max(HEADER_WRAP_LIMIT)),
-                Constraint::Min(self.longest_item_lens[9].max(HEADER_WRAP_LIMIT)),
-                Constraint::Min(self.longest_item_lens[10].max(HEADER_WRAP_LIMIT)),
-            
-            ],
-        )
-        .header(header)
-        .row_highlight_style(selected_row_style)
-        .column_highlight_style(selected_col_style)
-        .cell_highlight_style(selected_cell_style)
-        .highlight_symbol(Text::from(vec![
-            "".into(),
-            bar.into(),
-            bar.into(),
-            "".into(),
-        ]))
-        .bg(self.colors.buffer_bg)
-        .highlight_spacing(HighlightSpacing::Always);
-        frame.render_stateful_widget(t, area, &mut self.table_state);
-    }
-
-    fn render_scrollbar(&mut self, frame: &mut Frame, area: Rect) {
-        frame.render_stateful_widget(
-            Scrollbar::default()
-                .orientation(ScrollbarOrientation::VerticalRight)
-                .begin_symbol(None)
-                .end_symbol(None),
-            area.inner(Margin {
-                vertical: 1,
-                horizontal: 1,
-            }),
-            &mut self.scroll_state,
-        );
-    }
-
-    fn render_footer(&self, frame: &mut Frame, area: Rect) {       
-        let info_footer = Paragraph::new(Text::from_iter(INFO_TEXT))
-            .style(
-                Style::new()
-                    .fg(self.colors.row_fg)
-                    .bg(self.colors.buffer_bg),
-            )
-            .centered()
-            .block(
-                Block::bordered()
-                    .border_type(BorderType::Double)
-                    .border_style(Style::new().fg(self.colors.footer_border_color)),
-            );
-        frame.render_widget(info_footer, area);
     }
 
     fn exit(&mut self) {
@@ -358,9 +177,6 @@ impl<'a> App<'a> {
         }
     }
 
-    pub fn set_colors(&mut self) {
-        self.colors = TableColors::new(&PALETTES[self.color_index]);
-    }
 
     fn get_constraints(items: &Vec<Project>) -> [u16;11] {
         //find out mac unicode width of each column, or the header if the header is longer
@@ -380,6 +196,34 @@ impl<'a> App<'a> {
 
     
 }
+
+    //getters
+    pub fn get_table_state(&self) -> &TableState {&self.table_state}
+    pub fn get_data(&self) -> &Vec<Project> {&self.data}
+    pub fn get_exit(&self) -> bool {self.exit}
+    pub fn get_longest_item_lens(&self) -> &[u16; 11] {&self.longest_item_lens}
+    pub fn get_scroll_state(&self) -> &ScrollbarState {&self.scroll_state}
+    pub fn get_colors(&self) -> &TableColors {&self.colors}
+    pub fn get_color_index(&self) -> usize {self.color_index}
+    pub fn get_current_screen(&self) -> &CurrentScreen {&self.current_screen}
+    pub fn get_currently_editing(&self) -> &Option<CurrentlyEditing> {&self.currently_editing}
+    pub fn get_input_array(&self) -> &[&'a str; 11] {&self.input_array}
+
+    //mutable getters for states
+    pub fn get_mut_table_state(&mut self) -> &mut TableState {&mut self.table_state}
+    pub fn get_mut_scroll_state(&mut self) -> &mut ScrollbarState {&mut self.scroll_state}
+
+    //setters
+    pub fn set_table_state(&mut self, table_state: TableState) {self.table_state = table_state;}
+    pub fn set_data(&mut self, data: Vec<Project>) {self.data = data;}
+    pub fn set_longest_item_lens(&mut self, longest_item_lens: [u16; 11]) {self.longest_item_lens = longest_item_lens;}
+    pub fn set_scroll_state(&mut self, scroll_state: ScrollbarState) {self.scroll_state = scroll_state;}
+    pub fn set_input_array(&mut self, input_array: [&'a str; 11]) {self.input_array = input_array;}
+    pub fn set_exit(&mut self, exit: bool) {self.exit = exit;}
+    pub fn set_colors(&mut self, colors: TableColors) {self.colors = colors;}
+    pub fn set_color_index(&mut self, color_index: usize) {self.color_index = color_index;}
+    pub fn set_current_screen(&mut self, current_screen: CurrentScreen) {self.current_screen = current_screen;}
+    pub fn set_currently_editing(&mut self, currently_editing: Option<CurrentlyEditing>) {self.currently_editing = currently_editing;}
 }
 
 #[cfg(test)]
@@ -428,13 +272,5 @@ test_projects.push(Project::new(String::from("Dangle No. ".to_owned() + &i.to_st
             }
         }
     }
-    #[test]
-    fn line_wrap_test(){
-        let line_content = String::from("Lord-Veritant on Gryph-Stalker");
-
-        let cell_text = process_cell_content(line_content,CELL_WRAP_LIMIT);
-        assert_eq!(cell_text.lines.len(),2);
-        assert_eq!(cell_text.lines[0],Line::from("Lord-Veritant on"));
-        assert_eq!(cell_text.lines[1],Line::from("Gryph-Stalker"));
-    }
+   
 }
